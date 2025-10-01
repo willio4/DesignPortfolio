c79fff48bcbfa28069ed40ac1857ebe53eba4221
Patrick Baes <baesp1@unlv.nevada.edu>
2025-10-01 10:12:27 -0700

    constructors for claimed classes completed  -pat

--- Flask_project/beta.py
+++ Flask_project/beta.py
@@ -20,22 +20,34 @@ class MealPlanPrefs:
     def getPrefs(self):
         return self.prefs
     
-# Pat
+# ---------------------------------Pat-----------------------------------------
 # toDO:
 # set constructor , see UML diagram for more
 # no need to implment methods yet from UML
 # just implement a get mealplan method which returns all the meals set in constructor
 class MealPlan:
-    def __init__():
-        pass
+    def __init__(self, planID: int, approved: bool, user, meals):
+        self.planID = planID
+        self.approved = approved
+        self.user = user
+        self.meals = meals
+
+    def getMealPlan(self):
+        return self.meals
 
 # Pat
 # toDO:
 # set constructor , see UML diagram for more
 # for toString method just return a string the format '<nOfIngredient> <IngredientName> (s)'
 class Ingredient:
-    def __init__():
-        pass
+    def __init__(self, ingredientID: int, name: str, quantity: float, unit: str):
+        self.ingredientID = ingredientID
+        self.name = name
+        self.quantity = quantity
+        self.unit = unit
+    
+    def toString(self):
+        return f"{self.ingredientID} {self.name}/s"
 
 # Kenny
 # toDO:
