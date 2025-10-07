5b2547ec4bc96d627cf2c8e94feead4b2889ed15
kaos1184 <concept1184@gmail.com>
2025-10-06 23:43:44 -0700

    class User
    set up inputs for user Self with all the attributes needed to input data into chatGPT prompt

--- testbuild_0.3/Classes/User.py
+++ testbuild_0.3/Classes/User.py
@@ -10,12 +10,54 @@ class User:
     # 
 
     # Omar
-    def MealPlanPrefs():
+#    def MealPlanPrefs():
+#
+#       def __init__(self,bFast=None,lnch=None,dnr=None):
+#            preferences={"nBreakFast":bFast,"nLunch":lnch,"nDnr" :dnr}
+#            self.prefs=preferences
 
-        def __init__(self,bFast=None,lnch=None,dnr=None):
-            preferences={"nBreakFast":bFast,"nLunch":lnch,"nDnr" :dnr}
-            self.prefs=preferences
-
-        def getPrefs(self):
-            return self.prefs
+#        def getPrefs(self):
+#            return self.prefs
         
+
+ class User:
+
+
+    def __init__(self, name="", weight_lbs=0.0, height_ft=0, height_in=0,
+                 age=0, sex="M", goal="maintain"):
+
+        self._name = name
+        self._weight_lbs = float(weight_lbs)
+        self._height_ft = int(height_ft)
+        self._height_in = int(height_in)
+        self._age = int(age)
+        self._sex = sex.upper()
+        self._goal = goal.lower()
+
+    def _to_kg(self):
+        return self._weight_lbs * 0.45359237
+
+    def _to_cm(self):
+        total_inches = self._height_ft * 12 + self._height_in
+        return total_inches * 2.54
+
+    # getters
+    def getName(self):
+        return self._name
+
+    def getWeightLbs(self):
+        return self._weight_lbs
+
+    def getHeightFeetInches(self):
+        return (self._height_ft, self._height_in)
+
+    def getAge(self):
+        return self._age
+
+    def getSex(self):
+        return self._sex
+
+    def getGoal(self):
+        return self._goal
+
+    
