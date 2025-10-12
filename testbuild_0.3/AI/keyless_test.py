bbfdacc114e00b76c8348643d3d4e269d2222d5a
Tess <mulkeyt2@unlv.nevada.edu>
2025-10-11 19:32:06 -0700

    deleted obsolete file

--- testbuild_0.3/AI/keyless_test.py
+++
@@ -1,6 +0,0 @@
-#File to test files functionality within the AI folder, while excluding use
-#of API calls to OpenAI until ensuring basic functionality is working
-
-#This uses built-in "mock" library from `callModel` to simulate API calls
-
-import os 
