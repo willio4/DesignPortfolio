5c3f407df873801c47f4361b8f22ede7e2a3c313
Omar Williams <willio4@unlv.nevada.edu>
2025-10-20 16:39:58 -0700

    database implemented to store accounts/more html design

--- testbuild_0.3/Classes/User.py
+++ testbuild_0.3/Classes/User.py
@@ -1,24 +1,3 @@
-    # class to structure meal plan preferences from user inputs
-    # this will be populated from the main loop for now but in production will be populated from the ui
-
-    # toDO:
-        # add more user inputs that we could use for stage 1 of our product
-        # these will be what users see when we do our testing
-        # add then add those new parameters to this class constructor,
-    # if needed make lists for the parametrs to reduce the # of params in contructor
-    # 
-
-    # Omar
-#    def MealPlanPrefs():
-#
-#       def __init__(self,bFast=None,lnch=None,dnr=None):
-#            preferences={"nBreakFast":bFast,"nLunch":lnch,"nDnr" :dnr}
-#            self.prefs=preferences
-
-#        def getPrefs(self):
-#            return self.prefs
-        
-
 class User:
 
     DIETARY_RESTRICTIONS = [
