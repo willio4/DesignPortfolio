3001876430a8bb30f9bbba5897eae65ef09e9504
Kenny Huynh <huynhk6@unlv.nevada.edu>
2025-10-28 11:22:27 -0700

    Add GroceryList class skeleton with __init__ method and imports

--- testbuild_0.3/Classes/GroceryList.py
+++ testbuild_0.3/Classes/GroceryList.py
@@ -4,6 +4,11 @@
 # for the generate meal list method: return the Ingredient names with their quantities 
 # for the display method, call the generate meal list method,
 #  and print the result out to the console in a readable manner
+from Ingredient import Ingredient
+from MealPlan import MealPlan
+
 class GroceryList:
-    def __init__():
-        pass
+    def __init__(self, listID: int, user, ingredients=None):
+        self.listID = listID
+        self.user = user
+        self.ingredients = ingredients if ingredients is not None else []
